2f246a37dffd4b6b8902526d22d955d5e7dc4316
Ivan Kuznetsov <ikuzne@taltech.ee>
2024-04-10 14:15:47 +0300

    adds reg data into db

--- registration.php
+++ registration.php
@@ -2,7 +2,7 @@
 $arrErrors = [];
 if($_SERVER['REQUEST_METHOD'] === 'POST' && 
 isset($_POST['registerButton']) && $_POST['registerButton'] == "Register" &&
-!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['passwordConfirm'])) {
+!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['passwordConfirm']) && !empty($_POST['username'])) {
     $error = FALSE; //Defines whether there were any errors found.
     //Check email.
     if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
@@ -10,18 +10,7 @@ isset($_POST['registerButton']) && $_POST['registerButton'] == "Register" &&
         $emailError = "Invalid email address.";
         array_push($arrErrors, $emailError);
     }
-    //Check if user with this email already exists.
-    if ($error == FALSE && file_exists("data/users.csv")) {
-        $file = fopen("data/users.csv", "r");
-        while (($user = fgetcsv($file, 1000, ";")) !== FALSE) {
-            if ($user[0] == $_POST['email']) {
-                $error = TRUE;
-                $userExistsError = "User with this email is already registered.";
-                array_push($arrErrors, $userExistsError);
-                break;
-            }
-        }
-    }
+
     //Check password length.
     if (strlen($_POST['password']) < 8 || strlen($_POST['passwordConfirm'] < 8)) {
         $error = TRUE;
@@ -42,21 +31,67 @@ isset($_POST['registerButton']) && $_POST['registerButton'] == "Register" &&
         array_push($arrErrors, $passwordMatch);
     }
 
-    if ($error == FALSE) {
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $hashed_passwrod = password_hash($password, PASSWORD_DEFAULT);
-        $arrUser = ["email" => $email, "password" => $hashed_passwrod];
-       //Write to the file.
-        if (!file_exists("data/users.csv")) {
-            touch("data/users.csv");
-            chmod("data/users.csv", 0777);
-        }
-        $file = fopen("data/users.csv", "a");
-        fputcsv($file, $arrUser, ";");
-        fclose($file);
-        $confirmation = "Your account has been created.";
+    //Check username
+    if(!preg_match('/^[a-zA-Z0-9_-]{1,12}$/', $_POST['username']))
+    {
+        $error = TRUE;
+        $usernameError = "You can use letters, numbers, underscore, dash and max length is 12.";
+        array_push($arrErrors, $usernameError);
     }
+
+    //SQL STUFF
+    if ($error == FALSE) {
+        //Login into db
+        $server = "127.0.1.1"; // anysql.itcollege.ee
+        $user = "root"; // ICS0008_WT_4
+        $password = "1234"; // 14b9a69f0e86
+        $database = "ICS0008_4";
+        $mysqli = new mysqli($server, $user, $password, $database);
+        if ($mysqli->connect_error) {
+            echo"<script>alert('Connection to DB failed');</script>";
+            die;
+        }
+        else 
+        {
+            // Reg variables
+            $email = $_POST['email'];
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+            
+            
+            // Check if the user with the given email or username already exists
+            $query = "SELECT * FROM users WHERE email = ? OR username = ?";
+            $stmt = $mysqli->prepare($query);
+            $stmt->bind_param("ss", $email, $username);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            
+            if ($result->num_rows > 0) {
+                // User with the provided email or username already exists
+                $error = TRUE;
+                $userExistsError = "<p>User with this email or username already exists.</p>";
+                array_push($arrErrors, $userExistsError);
+            } else {
+                // User does not exist, add the user to the database
+                $query = "INSERT INTO users (email, username, pass) VALUES (?, ?, ?)";
+                $stmt = $mysqli->prepare($query);
+                $stmt->bind_param("sss", $email, $username, $hashed_password);
+                if ($stmt->execute()) {
+                    $confirmation = "<p>Your account has been created.</p>";
+                } else {
+                    $error = TRUE;
+                    $databaseAddingError = "<p>Error creating your account. Please try again later.</p>";
+                    array_push($arrErrors, $databaseAddingError);
+                }
+            }
+            
+            $stmt->close();
+        }
+    }
+
+
+
 }
 ?>
 <!DOCTYPE html>
@@ -78,6 +113,8 @@ isset($_POST['registerButton']) && $_POST['registerButton'] == "Register" &&
         <form method="post" id="registrationForm" name="registrationForm" action="registration.php">
             <h1 class="welcome-txt">Create an account</h1>
             <input class="form_field" type="email" id="email" name="email" placeholder="email" <?php if (isset($error) && $error == TRUE) {echo 'value="'.$_POST['email'].'"';} ?> required>
+            <br>
+            <input class="form_field" type="text" id="username" name="username" placeholder="username" pattern="[a-zA-Z0-9_-]{1,12}" required>
             <br>
             <input class="form_field" type="password" id="password" name="password" placeholder="password" required>
             <br>
