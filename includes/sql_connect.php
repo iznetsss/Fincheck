4737534b6afeb5d2cb7d3139b3d62fa07f83e76f
Ivan Kuznetsov <ikuzne@taltech.ee>
2024-04-26 23:47:49 +0300

    fixed sql connect

--- includes/sql_connect.php
+++ includes/sql_connect.php
@@ -1,4 +1,26 @@
 <?php
+$server = "anysql.itcollege.ee";
+$user = "ICS0008_WT_4";
+$password = "14b9a69f0e86";
+$database = "ICS0008_4";
+$link = new mysqli($server, $user, $password, $database);
+if ($link->connect_error) {
+    die("Connection to DB failed: " . $link->connect_error);
+}
+/*
+<?php
+$server = "anysql.itcollege.ee";
+$user = "ISC0008_WT_4";
+$password = "14b9a69f0e86";
+$database = "ICS0008_4";
+$link = new mysqli($server, $user, $password, $database);
+if ($link->connect_error) {
+    die("Connection to DB failed: " . $link->connect_error);
+}
+?>
+
+
+<?php
 $server = "127.0.0.1";
 $user = "root";
 $password = "1234";
@@ -7,4 +29,8 @@ $link = new mysqli($server, $user, $password, $database);
 if ($link->connect_error) {
     die("Connection to DB failed: " . $link->connect_error);
 }
+*/
+
 ?>
+
+
